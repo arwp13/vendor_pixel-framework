2fd48256f5313fc08f35860367cc98f609f59da1
Pranav Vashi <neobuddy89@gmail.com>
2024-04-16 02:53:17 +0900

    Clean up and update for A14 QPR2
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
    Co-authored-by: Joey <joey@evolution-x.org>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>

--- SystemUIGoogle/src/com/google/android/systemui/smartspace/BcSmartspaceCardWeatherForecast.java
+++ SystemUIGoogle/src/com/google/android/systemui/smartspace/BcSmartspaceCardWeatherForecast.java
@@ -5,6 +5,8 @@ import android.app.smartspace.SmartspaceTarget;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -13,7 +15,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.Constraints;
-import com.android.systemui.R;
+import com.android.systemui.res.R;
 import com.android.systemui.plugins.BcSmartspaceDataPlugin;
 import com.google.android.systemui.smartspace.logging.BcSmartspaceCardLoggingInfo;
 
@@ -79,6 +81,12 @@ public class BcSmartspaceCardWeatherForecast extends BcSmartspaceCardSecondary {
 
     @Override // com.google.android.systemui.smartspace.BcSmartspaceCardSecondary
     public final boolean setSmartspaceActions(SmartspaceTarget smartspaceTarget, BcSmartspaceDataPlugin.SmartspaceEventNotifier smartspaceEventNotifier, BcSmartspaceCardLoggingInfo bcSmartspaceCardLoggingInfo) {
+        boolean isWeatherEnabled = Settings.Secure.getIntForUser(getContext().getContentResolver(),
+                    Settings.Secure.LOCK_SCREEN_WEATHER_ENABLED,
+                    1, UserHandle.USER_CURRENT) != 0;
+        if (!isWeatherEnabled) {
+            return false;
+        }
         Bundle extras;
         SmartspaceAction baseAction = smartspaceTarget.getBaseAction();
         if (baseAction == null) {
--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
-# Inherit from pixel-framework config
-TARGET_INCLUDE_PIXEL_FRAMEWORKS ?= true
-ifeq ($(TARGET_INCLUDE_PIXEL_FRAMEWORKS),true)
 PRODUCT_PACKAGES += \
     SystemUIGoogle \
     SettingsGoogle
-endif
+
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUIGoogle \
+    SettingsGoogle
